22012277407fbfc88a0036b5d71ba36f957537bf
JulyTao <450480676@qq.com>
2022-10-23 13:53:38 +0000

    update src/main/java/org/jld/Main.java.
    Signed-off-by: JulyTao <450480676@qq.com>

--- src/main/java/org/jld/Main.java
+++ src/main/java/org/jld/Main.java
@@ -12,5 +12,6 @@ public class Main {
         System.out.println("New Fix222!");
         System.out.println("New Fix3333333333!");
         System.out.println("New Fix4444444444!");
+        System.out.println("New Fix5555555555!");
     }
 }
