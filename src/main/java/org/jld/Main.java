e7a3938a7b2087fd61c1c04e0fe0c2e1c0a0bc2b
LiTao <450480676@qq.com>
2022-10-23 16:52:59 +0800

    2 commit

--- src/main/java/org/jld/Main.java
+++ src/main/java/org/jld/Main.java
@@ -2,6 +2,7 @@ package org.jld;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello Git!");
         System.out.println("Hello world!");
     }
 }
